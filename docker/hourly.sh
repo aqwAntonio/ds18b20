106262a22463b222a38e7e4ab1b0aa6c8a0e7e96
anton <anton@localhost.localdomain>
2024-02-08 15:59:33 +0500

    local changes

---
+++ docker/hourly.sh
@@ -0,0 +1,9 @@
+db_status=$(podman inspect home-db --format "{{.State.Status}}")
+if [[ $db_status != "running" ]]
+then
+	podman start home-db
+	sleep 10
+	podman start home-python
+else
+	podman start home-python
+fi
